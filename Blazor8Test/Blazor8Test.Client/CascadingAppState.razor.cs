c4b9bf8fe539f4e520e427d8f14139665dfd07b7
Carl Franklin <carl@appvnext.com>
2024-01-19 07:52:22 -0500

    Discovered a bug. Using per page/component auto render mode, cascading parameters turn null after rendering.

---
+++ Blazor8Test/Blazor8Test.Client/CascadingAppState.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor8Test.Client;
+
+public partial class CascadingAppState : ComponentBase
+{
+    [Parameter]
+    public RenderFragment ChildContent { get; set; }
+
+    private int count = 0;
+    public int Count
+    {
+        get => count;
+        set
+        {
+            count = value;
+            StateHasChanged();
+        }
+    }
+
+}
